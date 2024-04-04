bc878c889db28b4c502872419d5068dd8bc44150
mr-induction <130118198+mr-induction@users.noreply.github.com>
2024-04-04 13:29:56 -0500

    Update Header.jsx

--- app/src/components/Header.jsx
+++ app/src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header({onNewChat}) {
     return (
         <div className="flex flex-row p-4 bg-slate-500 rounded-xl my-4">
-            <p className="text-3xl text-slate-200 font-semibold grow">KieCodesGPT</p>
+            <p className="text-3xl text-slate-200 font-semibold grow">LogicGPT</p>
             <button
                 className="bg-slate-400 hover:bg-pink-400 text-white font-bold py-2 px-4 rounded"
                 onClick={onNewChat}
